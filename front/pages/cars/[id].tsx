d7d339581463a611a113b4f445bb3cb19d9b68d6
Akshit <amalhotra@paymentus.com>
2021-11-15 10:13:24 +0530

    initial

--- front/pages/cars/[id].tsx
+++ front/pages/cars/[id].tsx
@@ -3,9 +3,7 @@ import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import Link from 'next/link'
 import { Icar } from '../../types'
 
-interface CarProps {
-    carinfo:Icar
-}
+
 
 // export const getStaticPaths = async ()=> {
 //     const res = await fetch(`http://localhost:8000/car`)
@@ -47,51 +45,35 @@ interface CarProps {
 
 // export default Detail
 
-const Detail = async({carinfo}:CarProps)=>{
-  console.log("carinfo",carinfo)
-      return (
+import React, {useState,useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+
+export default function Detail(){
+  const [carBrand,setcarBrand] = useState("");
+  const [carColor,setcarColor] = useState("");
+  const [carModel,setcarModel] = useState("");
+
+  const {id} = useParams();
+  
+  useEffect(()=>{
+    fetch(`http://localhost:8000/car/${id}`)
+    .then(res=>res.json())
+    .then((result)=>{
+      setcarBrand(result.brand)
+      setcarColor(result.color)
+      setcarModel(result.model)
+    })
+},[])
+
+
+return (
           <div>
-            <h2>Brand = {carinfo.brand}</h2>
-            <h2>Color = {carinfo.color}</h2>
-            <h2>Model = {carinfo.model}</h2>
+            <h2>Brand = {carBrand}</h2>
+            <h2>Color = {carColor}</h2>
+            <h2>Model = {carModel}</h2>
   
             <Link href="/cars">see all cars</Link>
           </div>
         )
-  }
-  
-  export default Detail
 
-  export const getStaticProps = async(context: { params: { id: number } })=>{
-        const id = context.params.id;
-        const res = await fetch('http://localhost:8000/car/'+id)
-        const data = await res.json();
-        return{
-            props:{carinfo:data}
-        }
-    }
-
-    export const getStaticPaths = async ()=> {
-          const res = await fetch(`http://localhost:8000/car`)
-          const data = await res.json()
-          
-          const paths = data.map((car: { id: { toString: () => any } })=>{
-              return{
-                  params:{id:car.id.toString()}
-              }
-          })
-        
-          return {
-            paths,
-            fallback:false
-          }
-        }
-
-
-      //   export async function getServerSideProps({params}:GetServerSidePropsContext){
-      //     const res = await fetch(`localhost:8000/car/${params?.id}`)
-      //     const data = await res.json();
-      //     return{
-      //         props:{carinfo:data}
-      //     }
-      // }
+}
